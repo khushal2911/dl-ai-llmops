110b6a6ea5e1b42edbbd39746236410dd9ede302
Khushal Kharade <khushal.kharade@gmail.com>
2025-06-13 20:51:16 +0000

    updated RLHF peft notebook

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from google.oauth2.service_account import Credentials
 def authenticate():
     #return "DLAI_CREDENTIALS", "DLAI_PROJECT_ID"
     # Path to the json key associated with your service account from google cloud
-    key_path = './.env/llmops-sa-key.json'
+    key_path = './.env/llmops-462607-305d3f0929d9.json'
     # Create credentials from service account file
     credentials = Credentials.from_service_account_file(
         key_path,
